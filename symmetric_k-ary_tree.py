f7bfaa456a91dd4939e3fe21e62c8e225df777ef
asingh369 <amritpalsinghs369@gmail.com>
2019-09-27 00:21:28 -0700

    Initial k-ary solution

--- symmetric_k-ary_tree.py
+++ symmetric_k-ary_tree.py
@@ -17,19 +17,34 @@ class Node():
 
 def are_mirror(root1, root2):
   if root1.value != root2.value:
+    print(root1.value, root2.value)
+
     return False
   if len(root1.children) != len(root2.children):
+    print(2)
     return False
   
   for i in range(len(root1.children)):
     if not are_mirror(root1.children[i], root2.children[len(root1.children)-i-1]):
+      print(3)
       return False
 
   return True
 
 def is_symmetric(root):
-  if len(root.children) != 2:
-    return False
+  if len(root.children)%2 == 0:
+    for i in range(len(root.children)//2):
+      if not are_mirror(root.children[i], root.children[len(root.children)-i-1]):
+        print(4)
+        return False
+    return True
+  else:
+    for i in range(len(root.children)//2):
+      if not are_mirror(root.children[i], root.children[len(root.children)-i-1]):
+        print(root.value)
+        return False
+    return is_symmetric(root.children[len(root.children)//2 + 1])
+
   return are_mirror(root.children[0], root.children[1])
 
 tree = Node(4)
@@ -38,3 +53,10 @@ tree.children[0].children = [Node(9), Node(4), Node(1)]
 tree.children[1].children = [Node(1), Node(4), Node(9)]
 print(is_symmetric(tree))
 # True
+
+tree2 = Node(1)
+tree2.children = [Node(2), Node(3), Node(2)]
+tree2.children[0].children = [Node(9), Node(4), Node(1)]
+tree2.children[2].children = [Node(1), Node(4), Node(9)]
+
+print(is_symmetric(tree2))
