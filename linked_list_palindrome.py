131109be373e58a359dba1e8cae04cecac48b5b3
asingh369 <amritpalsinghs369@gmail.com>
2019-10-11 00:05:41 -0700

    Determine If Linked List is Palindrome

--- linked_list_palindrome.py
+++ linked_list_palindrome.py
@@ -9,7 +9,30 @@ class Node(object):
     self.prev = None
 
 def is_palindrome(node):
-  pass
+  if node == None:
+    return True
+
+  current = node
+  while current.next != None:
+    current = current.next
+
+  start = node
+  end = current
+
+  while start != end:
+    if start.val != end.val:
+      return False
+    else:
+      if start.next == end:
+        if start.next.val == end.val:
+          return True
+        else:
+          return False
+      start = start.next
+      end = end.prev
+
+  return True
+
 
 node = Node('a')
 node.next = Node('b')
@@ -19,5 +42,5 @@ node.next.next.prev = node.next
 node.next.next.next = Node('a')
 node.next.next.next.prev = node.next.next
 
-print is_palindrome(node)
+print(is_palindrome(node))
 # True
