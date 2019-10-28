ea853885211c267b35dec74fe80bb01680facd27
asingh369 <amritpalsinghs369@gmail.com>
2019-10-28 13:28:04 -0700

    palindrome integers

--- palindrome_integers.py
+++ palindrome_integers.py
@@ -5,8 +5,18 @@ Given an integer, check if that integer is a palindrome. For this problem do not
 import math
 
 def is_palindrome(n):
-  # Fill this in.
-  pass
+  copy_n = n
+  reverse_n = 0
+
+
+  while copy_n > 0:
+    reverse_n = reverse_n * 10 + (copy_n % 10)
+    copy_n = copy_n // 10
+
+  if reverse_n == n:
+    return True
+  return False
+
 
 print(is_palindrome(1234321))
 # True
