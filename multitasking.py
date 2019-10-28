e6cac07dbd6db5879a7285c29ad0b1e6e763803d
asingh369 <amritpalsinghs369@gmail.com>
2019-10-28 14:18:19 -0700

    multitasking

--- multitasking.py
+++ multitasking.py
@@ -7,9 +7,29 @@ cooldown = 2
 output: 7 (order is 1 _ _ 1 2 _ 1)
 """
 
-def findTime(arr, cooldown):
-  # Fill this in.
-  pass
+def is_cooling_down(cooling_down, task):
+  result = False
+  for i in cooling_down:
+    i[1] = i[1] - 1
+    if i[0] == task:
+      result = True
+    if i[1] <= 0:
+      cooling_down.remove(i)
+  return result
 
-print(findTime([1, 1, 2, 1], 2))
+def findTime(arr, cooldown):
+  time = 0
+  cooling_down = []
+  how_it_happened = ''
+  for i in arr:
+    while is_cooling_down(cooling_down, i):
+      how_it_happened = how_it_happened + '_ '
+      time = time + 1
+    cooling_down.append([i, cooldown])
+    how_it_happened = how_it_happened + str(i) + ' '
+    time = time + 1
+  print(how_it_happened)
+  return time 
+
+print(findTime([1, 1, 2, 1], 3))
 # 7
