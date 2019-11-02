0ef58a2c0ca4e9fbf17912a8c298abc27a3679a4
asingh369 <amritpalsinghs369@gmail.com>
2019-11-01 22:25:00 -0700

    kaprekar constant solution

--- interview_problems/facebook/kaprekar_constant.py
+++ interview_problems/facebook/kaprekar_constant.py
@@ -4,7 +4,25 @@
 
 KAPREKAR_CONSTANT = 6174
 
-def number_kaprekar_iterations(n):
-  pass
+
+def sort_ascending(n):
+  n = ''.join(sorted(str(n)))
+  return int(n)
+
+def sort_descending(n):
+  n = ''.join(sorted(str(n)))[::-1]
+  if len(n) < 4:
+    for i in range(len(n), 4):
+      n = n + '0'
+  return int(n)
+
+
+def number_kaprekar_iterations(n, count=1):
+  ascending = sort_ascending(n)
+  descending = sort_descending(n)
+  diff = descending - ascending
+  if diff == KAPREKAR_CONSTANT:
+    return count
+  return number_kaprekar_iterations(diff, count+1)
 
 print(number_kaprekar_iterations(123))
